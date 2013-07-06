6af8f01a2b3f3446df73c3e9d2f58a42f80060f9
Fortune.Zhang <zhangfortune527@gmail.com>
2013-07-06 13:55:31 +0800

    gitignore

--- src/gen/R.java.d
+++
@@ -1,7 +0,0 @@
-/home/fortunezhang/learn/demo02/src/gen/com/learn/demo02/R.java \
- : /home/fortunezhang/learn/demo02/src/res/drawable-mdpi/ic_launcher.png \
-/home/fortunezhang/learn/demo02/src/res/drawable-hdpi/ic_launcher.png \
-/home/fortunezhang/learn/demo02/src/res/drawable-ldpi/ic_launcher.png \
-/home/fortunezhang/learn/demo02/src/res/layout/main.xml \
-/home/fortunezhang/learn/demo02/src/res/values/strings.xml \
-/home/fortunezhang/learn/demo02/src/bin/AndroidManifest.xml \
